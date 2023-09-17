0ea76454c87985fd1184a8656f06502f7a57f2cf
LimboHunter <user36976@st.guap.ru>
2023-09-17 23:52:02 +0300

    added ingedients and lombok dependecy

---
+++ src/main/java/pizzas/Pizza.java
@@ -0,0 +1,9 @@
+package pizzas;
+
+import java.util.List;
+import lombok.Data;
+@Data
+public class Pizza {
+    private String name;
+    private List<Ingredient> ingredients;
+}
---
+++ src/main/java/pizzas/PizzaOrder.java
@@ -0,0 +1,21 @@
+package pizzas;
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+@Data
+public class PizzaOrder {
+    private String deliveryName;
+    private String deliverySteet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Pizza> pizzas = new ArrayList<>();
+
+    public void addPizza(Pizza pizza){
+        this.pizzas.add(pizza);
+    }
+}
