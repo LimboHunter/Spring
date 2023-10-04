11c7684fc1204884d9a1e355ea365b94401d79f3
LimboHunter <user36976@st.guap.ru>
2023-10-04 23:36:50 +0300

    Again added security, at least it works (For now) hope it will do so after adding user saving

---
+++ src/main/java/pizzas/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package pizzas.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new StandardPasswordEncoder("53cr3t");
+    }
+
+    public interface UserDetailsService {
+        UserDetails loadUserByUsername(String username) throws
+                UsernameNotFoundException;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(new User(
+                "buzz", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User(
+                "woody", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(usersList);
+    }
+}
