83628d62ad1dc9966da2aaf59a40ced37229db03
LimboHunter <user36976@st.guap.ru>
2023-10-06 21:00:29 +0300

    Finally finished registration with DB and login (I fckng forgot about methods that was returning null instead of data, so i just deleted them). Finally can authorize, but still have 'Forbidden' on orders page.

--- src/main/java/pizzas/User.java
+++ src/main/java/pizzas/User.java
@@ -10,9 +10,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-@Table
 @Data
+@Table
 @RequiredArgsConstructor
 public class User implements UserDetails {
 
@@ -29,20 +28,9 @@ public class User implements UserDetails {
     private final String state;
     private final String zip;
     private final String phoneNumber;
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
     }
 
     @Override
