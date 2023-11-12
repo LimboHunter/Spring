7a6f7cab20c27875bd2008a93d470fa96bce1d2d
LimboHunter <user36976@st.guap.ru>
2023-11-12 22:54:33 +0300

    Made some starter controllers for REST ended on 7.2 of book

---
+++ src/main/java/pizzas/web/api/PizzaController.java
@@ -0,0 +1,42 @@
+package pizzas.web.api;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import pizzas.Pizza;
+import pizzas.PizzaOrder;
+import pizzas.data.PizzaRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/pizzas",
+produces = "application/json")
+@CrossOrigin(origins = "http://pizzacloud:8080")
+public class PizzaController {
+
+    private PizzaRepository pizzaRepo;
+
+    public PizzaController(PizzaRepository pizzaRepo){
+        this.pizzaRepo = pizzaRepo;
+    }
+
+/*    @GetMapping(params = "recent")
+    public Iterable<Pizza> recentPizzas() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return pizzaRepo.findAll(page).getContent();
+    }*/
+    @GetMapping("/{id}")
+    public Optional<Pizza> pizzaById(@PathVariable("id") Long id){
+        return pizzaRepo.findById(id);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Pizza postPizza(@RequestBody Pizza pizza){
+        return pizzaRepo.save(pizza);
+    }
+}
